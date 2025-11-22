e967f96241b5d4469b2ab3c88080a4e0d1d58a22
Viacheslav <v.a.asadchiy@gmail.com>
2025-11-22 16:50:11 +0700

    style: apply spotless

--- src/main/java/academy/model/LogAnalysisResult.java
+++ src/main/java/academy/model/LogAnalysisResult.java
@@ -3,20 +3,11 @@ package academy.model;
 import java.util.List;
 
 public record LogAnalysisResult(
-        List<String> files,
-        long totalRequestsCount,
-        academy.model.LogAnalysisResult.ResponseSizeStats responseSizeInBytes,
-        List<ResourceStat> resources,
-        List<ResponseCodeStat> responseCodes,
-        List<DailyRequestStat> requestsPerDate,
-        List<String> uniqueProtocols) {
-
-    public record ResponseSizeStats(double average, double max, double p95) {}
-
-    public record ResourceStat(String resource, long totalRequestsCount) {}
-
-    public record ResponseCodeStat(int code, long totalResponsesCount) {}
-
-    public record DailyRequestStat(
-            String date, String weekday, long totalRequestsCount, double totalRequestsPercentage) {}
+    List<String> files,
+    long totalRequestsCount,
+    ResponseSizeStats responseSizeInBytes,
+    List<ResourceStat> resources,
+    List<ResponseCodeStat> responseCodes,
+    List<DailyRequestStat> requestsPerDate,
+    List<String> uniqueProtocols) {
 }
