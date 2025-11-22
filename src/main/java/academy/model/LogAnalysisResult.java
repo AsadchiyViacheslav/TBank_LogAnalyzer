caa6c8ff32656b8cef9226be12bca46443b441ff
Viacheslav <v.a.asadchiy@gmail.com>
2025-11-22 15:15:09 +0700

    refactor: make models record classes

--- src/main/java/academy/model/LogAnalysisResult.java
+++ src/main/java/academy/model/LogAnalysisResult.java
@@ -1,50 +1,22 @@
 package academy.model;
 
-import lombok.AllArgsConstructor;
-import lombok.Getter;
-
 import java.util.List;
-import java.util.Map;
 
-@Getter
-@AllArgsConstructor
-public class LogAnalysisResult {
-    private final List<String> files;
-    private final long totalRequestsCount;
-    private final ResponseSizeStats responseSizeInBytes;
-    private final List<ResourceStat> resources;
-    private final List<ResponseCodeStat> responseCodes;
-    private final List<DailyRequestStat> requestsPerDate;
-    private final List<String> uniqueProtocols;
+public record LogAnalysisResult(
+        List<String> files,
+        long totalRequestsCount,
+        academy.model.LogAnalysisResult.ResponseSizeStats responseSizeInBytes,
+        List<ResourceStat> resources,
+        List<ResponseCodeStat> responseCodes,
+        List<DailyRequestStat> requestsPerDate,
+        List<String> uniqueProtocols) {
 
-    @Getter
-    @AllArgsConstructor
-    public static class ResponseSizeStats {
-        private final double average;
-        private final double max;
-        private final double p95;
-    }
+    public record ResponseSizeStats(double average, double max, double p95) {}
 
-    @Getter
-    @AllArgsConstructor
-    public static class ResourceStat {
-        private final String resource;
-        private final long totalRequestsCount;
-    }
+    public record ResourceStat(String resource, long totalRequestsCount) {}
 
-    @Getter
-    @AllArgsConstructor
-    public static class ResponseCodeStat {
-        private final int code;
-        private final long totalResponsesCount;
-    }
+    public record ResponseCodeStat(int code, long totalResponsesCount) {}
 
-    @Getter
-    @AllArgsConstructor
-    public static class DailyRequestStat {
-        private final String date;
-        private final String weekday;
-        private final long totalRequestsCount;
-        private final double totalRequestsPercentage;
-    }
+    public record DailyRequestStat(
+            String date, String weekday, long totalRequestsCount, double totalRequestsPercentage) {}
 }
--- src/main/java/academy/model/NginxLogEntry.java
+++ src/main/java/academy/model/NginxLogEntry.java
@@ -1,18 +1,12 @@
 package academy.model;
 
-import lombok.AllArgsConstructor;
-import lombok.Getter;
-
 import java.time.LocalDateTime;
 
-@Getter
-@AllArgsConstructor
-public class NginxLogEntry {
-    private final String remoteAddr;
-    private final LocalDateTime timestamp;
-    private final String method;
-    private final String resource;
-    private final String protocol;
-    private final int statusCode;
-    private final long bodyBytes;
-}
+public record NginxLogEntry(
+        String remoteAddr,
+        LocalDateTime timestamp,
+        String method,
+        String resource,
+        String protocol,
+        int statusCode,
+        long bodyBytes) {}
